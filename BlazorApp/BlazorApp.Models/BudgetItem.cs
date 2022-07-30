32cf9aa787519a6d176428d96f5d8e903d49b712
kyounghoon <kkh2826@naver.com>
2022-07-30 14:41:52 +0900

    Divide Budget App

---
+++ BlazorApp/BlazorApp.Models/BudgetItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class BudgetItem
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
