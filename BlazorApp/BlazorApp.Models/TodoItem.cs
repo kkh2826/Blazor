6c1b891376e42aca19f90d3d52f30f9ba2e9f81e
kyounghoon <kkh2826@naver.com>
2022-07-30 14:10:44 +0900

    Basic TodoList

---
+++ BlazorApp/BlazorApp.Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+    }
+}
