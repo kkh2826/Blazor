c8312bea403b046d86fbed27d6bd07b29762cf8a
kyounghoon <kkh2826@naver.com>
2022-07-31 13:10:07 +0900

    Apply NavigationManager

---
+++ BlazorPractice/BlazorPractice/Pages/Portfolios/PortfolioWebsite.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorPractice.Pages.Portfolios
+{
+    public partial class PortfolioWebsite
+    {
+        [Inject]
+        public NavigationManager NavigationManagerReference { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected void GoToDotNetKorea()
+        {
+            NavigationManagerReference.NavigateTo("/");
+        }
+    }
+}
