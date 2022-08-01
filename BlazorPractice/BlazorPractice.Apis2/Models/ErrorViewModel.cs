4de89fc9c3077ce74fa6fb1be38dd42ec0c2ad5a
kyounghoon <kkh2826@naver.com>
2022-08-01 20:30:33 +0900

    Add API Web And Mutliple Supplies

---
+++ BlazorPractice/BlazorPractice.Apis2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlazorPractice.Apis2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
