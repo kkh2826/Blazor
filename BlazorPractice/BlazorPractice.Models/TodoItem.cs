be3e0e2de2df529457ae2d7c9f61d472b6101c4c
kyounghoon <kkh2826@naver.com>
2022-08-01 21:05:49 +0900

    Apply Bootstrap Tab

---
+++ BlazorPractice/BlazorPractice.Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace BlazorPractice.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
