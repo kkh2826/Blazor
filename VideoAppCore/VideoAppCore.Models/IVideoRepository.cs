9f982c411ccdb77d27ca58b3657d234c76431dbc
kyounghoon <kkh2826@naver.com>
2022-08-02 20:49:17 +0900

    Add Video Basic Interface

---
+++ VideoAppCore/VideoAppCore.Models/IVideoRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace VideoAppCore.Models
+{
+    public interface IVideoRepository
+    {
+        Video AddVideo(Video model);        // 입력
+        List<Video> GetVideos();            // 출력
+        Video GetVideoById(int id);         // 상세
+        Video UpdateVideo(Video model);     // 수정
+        void RemoveVideo(int id);           // 삭제
+    }
+}
