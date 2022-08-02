12b8ff1d9bd97135cd8c8bb652d917cc6185fefc
kyounghoon <kkh2826@naver.com>
2022-08-02 20:56:28 +0900

    Divide Interface By Repository

---
+++ VideoAppCore/VideoAppCore.Models/VideoRepositoryAdoNet.cs
@@ -0,0 +1,30 @@
+﻿namespace VideoAppCore.Models
+{
+    public class VideoRepositoryAdoNet : IVideoRepository
+    {
+        public Video AddVideo(Video model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video GetVideoById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Video> GetVideos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveVideo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video UpdateVideo(Video model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
