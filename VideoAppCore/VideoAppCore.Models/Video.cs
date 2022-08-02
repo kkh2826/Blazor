c2d438e4a3e590b925348fb153eebe93c3a601b4
kyounghoon <kkh2826@naver.com>
2022-08-02 20:40:18 +0900

    Video Class Made

---
+++ VideoAppCore/VideoAppCore.Models/Video.cs
@@ -0,0 +1,12 @@
+﻿namespace VideoAppCore.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+}
