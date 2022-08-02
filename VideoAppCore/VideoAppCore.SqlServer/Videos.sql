8e7bc12ecc6ac217ccc65a0208ef1a957ae1d974
kyounghoon <kkh2826@naver.com>
2022-08-02 20:27:26 +0900

    SQL Setting

---
+++ VideoAppCore/VideoAppCore.SqlServer/Videos.sql
@@ -0,0 +1,13 @@
+﻿
+-- 비디오 테이블
+CREATE TABLE [dbo].[Videos]
+(
+	[Id] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	[Created] Datetimeoffset(7) DEFAULT(SYSDATETIMEOFFSET() AT TIME ZONE 'Korea Standard Time'),
+	[Title] NVARCHAR(255) NOT NULL,	-- 제목
+	[Url] NVARCHAR(MAX) NULL,		-- URL
+	[Name] NVARCHAR(50) NULL,		-- 이름
+	[Company] NVARCHAR(255) NULL,	-- 회사
+)
+GO
+
