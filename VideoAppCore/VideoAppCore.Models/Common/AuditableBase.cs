6a8580e8068958d82549eed7c36b8194644e0bce
kyounghoon <kkh2826@naver.com>
2022-08-03 21:42:23 +0900

    Video DTO Edit

--- VideoAppCore/VideoAppCore.Models/Common/AuditableBase.cs
+++ VideoAppCore/VideoAppCore.Models/Common/AuditableBase.cs
@@ -2,11 +2,11 @@
 {
     public class AuditableBase
     {
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
 
-        public DateTime Created { get; set; }
+        public DateTime? Created { get; set; }
 
-        public string ModifiedBy { get; set; }
+        public string? ModifiedBy { get; set; }
 
         public DateTime? Modified { get; set; }
     }
--- VideoAppCore/VideoAppCore.Models/Video.cs
+++ VideoAppCore/VideoAppCore.Models/Video.cs
@@ -6,9 +6,9 @@ namespace VideoAppCore.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Url { get; set; }
-        public string Name { get; set; }
-        public string Company { get; set; }
+        public string? Url { get; set; }
+        public string? Name { get; set; }
+        public string? Company { get; set; }
 
         
     }
