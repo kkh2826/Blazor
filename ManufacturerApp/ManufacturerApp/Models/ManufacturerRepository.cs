5541f23b66cb1bc38faa16ee238486dfbb85fbb6
kyounghoon <kkh2826@naver.com>
2022-08-04 21:05:12 +0900

    Make Repository Action

--- ManufacturerApp/ManufacturerApp/Models/ManufacturerRepository.cs
+++ ManufacturerApp/ManufacturerApp/Models/ManufacturerRepository.cs
@@ -1,30 +1,49 @@
-﻿namespace ManufacturerApp.Models
+﻿using ManufacturerApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManufacturerApp.Models
 {
     public class ManufacturerRepository : IManufacturerRepository
     {
-        public Task<Manufacturer> AddManufacturer(Manufacturer manufacturer)
+        private readonly ApplicationDbContext _context;
+        public ManufacturerRepository(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            this._context = context;
+        }
+        public async Task<Manufacturer> AddManufacturer(Manufacturer manufacturer)
+        {
+            _context.Manufacturers.Add(manufacturer);
+            await _context.SaveChangesAsync();
+
+            return manufacturer;
+        }
+        public async Task<List<Manufacturer>> GetManufacturers()
+        {
+            return await _context.Manufacturers.OrderBy(m => m.Id).ToListAsync();
         }
 
-        public Task DeleteManufacturer(int id)
+        public async Task<Manufacturer> GetManufacturer(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Manufacturers.Where(m => m.Id == id).SingleOrDefaultAsync();
+        }
+        public async Task<Manufacturer> EditGetManufacturer(Manufacturer manufacturer)
+        {
+            _context.Entry(manufacturer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return manufacturer;
+        }
+        public async Task DeleteManufacturer(int id)
+        {
+            var manufacturer = await _context.Manufacturers.Where(m => m.Id == id).SingleOrDefaultAsync();
+            if (manufacturer != null)
+            {
+                _context.Manufacturers.Remove(manufacturer);
+                await _context.SaveChangesAsync();
+            }
+            
         }
 
-        public Task<Manufacturer> EditGetManufacturer(Manufacturer manufacturer)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<Manufacturer> GetManufacturer(int id)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<List<Manufacturer>> GetManufacturers()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
