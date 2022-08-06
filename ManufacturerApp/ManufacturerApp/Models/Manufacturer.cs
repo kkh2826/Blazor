7779e2d4f353597ecfeaa0fbcf586a3e9dc0c35e
kyounghoon <kkh2826@naver.com>
2022-08-06 15:25:58 +0900

    Apply CRUD Manufacturer

--- ManufacturerApp/ManufacturerApp/Models/Manufacturer.cs
+++ ManufacturerApp/ManufacturerApp/Models/Manufacturer.cs
@@ -1,9 +1,13 @@
-﻿namespace ManufacturerApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManufacturerApp.Models
 {
     public class Manufacturer
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string ManufacturerCode { get; set; }
         public string Comment { get; set; }
     }
